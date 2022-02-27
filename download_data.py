c01656e2868e80ef7b7ee87103f198062604c54c
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2022-02-27 16:30:16 -0500

    Download all contests from 660

--- download_data.py
+++ download_data.py
@@ -1,13 +1,17 @@
 # Imports
 import argparse
 import asyncio
+import json
 import pdb
 import re
+from shutil import rmtree
 
-import aiofiles
 import aiohttp
 from aiohttp import ClientSession
+from aiopath import AsyncPath
+from tqdm.asyncio import tqdm_asyncio
 
+from util.asyncio import to_thread
 from util.logconf import logging
 
 # Configs
@@ -56,6 +60,10 @@ class DownloadDataScript:
             "contests": [self.urlize(contest_id) for contest_id in self.contest_ids],
             "winners": f"{self.prefix}/nyccwinners/nyc_winners.json",
         }
+        # Will be assigned an aiohttp.ClientSession object in the download function
+        self.session = None
+        # Will be assigned a dict of winning captions for each contest
+        self.winners = None
 
     def main(self):
         """
@@ -81,6 +89,7 @@ class DownloadDataScript:
         """
         prefix = self.prefix
         return {
+            "id": contest_id,
             "cartoon": f"{prefix}/cartoons/{contest_id}.jpg",
             "captions": f"{prefix}/summaries/{contest_id}.csv",
         }
@@ -90,27 +99,116 @@ class DownloadDataScript:
         Downloading script.
         """
         async with ClientSession() as session:
+            # Pass session object as a class attribute so that we don't have to keep
+            # calling it in the download and fetch functions below. Since self.session
+            # refers to the same ClientSession object as session, it will be closed
+            # when we exit this context manager
+            self.session = session
             # All contest winners are in the same file, so download that file first
-            winners = await self.download_winners(
-                url=self.urls["winners"], session=session
-            )
-            # For each contest, download its cartoon and captions, then append its winners
-            await self.download_contests(
-                url=self.urls["contests"], winners=winners, session=session
-            )
+            self.winners = await self.fetch_winners(self.urls["winners"])
+            # For each contest, download its cartoon and captions, then append its
+            # winners and write into files
+            tasks = [
+                self.fetch_and_write_contest(contest_urls)
+                for contest_urls in self.urls["contests"]
+            ]
+            await tqdm_asyncio.gather(*tasks)
 
-    async def download_winners(self, url, session):
+    async def fetch_winners(self, url):
         """
         Fetch and parse JSON of contest winning captions.
         """
-        winners = {"unavail_id": []}
+        response = await self.fetch(url)
+        if response is None:
+            logger.info(
+                "An exception was encountered while retrieving contest winners. No data will be returned."
+            )
+            return None
 
+        winners = {"unavail_id": []}
+        data = await response.json(content_type="text/plain")
+
+        # Append contest ID as a key-value pair
+        for d in data:
+            # Assuming d["data"] and d["data"]["cartoon"] are always available
+            contest = d["data"]["cartoon"]
+            # Fetch ID (e.g., 660) from contest title (e.g., Contest #660)
+            contest_id = re.search(r"#\d+", contest["title"])
+            if contest_id is not None:
+                contest_id = int(contest_id.group()[1:])
+                winners[contest_id] = d
+            else:
+                logger.warning(
+                    "Unable to find or parse contest ID from this JSON object: %s",
+                    d,
+                )
+                winners["unavail_id"].append(d)
+
+        return winners
+
+    async def fetch_and_write_contest(self, urls):
+        """
+        Fetch and write cartoon, captions and winners of a contest into files.
+        """
+        contest_id = urls["id"]
+
+        path_contest = AsyncPath(f"./data/{contest_id}")
+        override = self.cli_args.override
+        path_contest_exists = await path_contest.exists()
+
+        # Skip if --override is not set and path already exists
+        if override is False and path_contest_exists is True:
+            return
+
+        # If path already exists and --override is set, delete directory
+        if path_contest_exists is True:
+            # Assuming path is always a directory
+            await to_thread(rmtree, path_contest)
+
+        # Send requests
+        url_cartoon = urls["cartoon"]
+        url_captions = urls["captions"]
+        response_cartoon, response_captions = await asyncio.gather(
+            self.fetch(url_cartoon), self.fetch(url_captions)
+        )
+
+        # Validation check
+        if response_cartoon is None or response_captions is None:
+            logger.info(
+                "An exception was encountered while retrieving data for contest #%s. No data will be returned.",
+                contest_id,
+            )
+            return
+
+        # Grab content from responses in bytes
+        content_cartoon, content_captions = await asyncio.gather(
+            response_cartoon.read(), response_captions.read()
+        )
+        # Contest winners are stored in text
+        content_winners = await to_thread(json.dumps, self.winners.get(contest_id, {}))
+
+        # Re-create contest directory
+        await path_contest.mkdir()
+
+        # Write cartoon, caption and winner data to respective files
+        path_cartoon = AsyncPath(f"{str(path_contest)}/cartoon.jpg")
+        path_captions = AsyncPath(f"{str(path_contest)}/captions.csv")
+        path_winners = AsyncPath(f"{str(path_contest)}/winners.json")
+
+        await asyncio.gather(
+            path_cartoon.write_bytes(content_cartoon),
+            path_captions.write_bytes(content_captions),
+            path_winners.write_text(content_winners),
+        )
+
+    async def fetch(self, url):
+        """
+        Helper that sends a request to a URL and returns a response or handles errors.
+        """
         try:
-            response = await session.request("GET", url)
+            response = await self.session.request("GET", url)
             # Raise an aiohttp.ClientResponseError if the response status is 400 or higher.
             response.raise_for_status()
-            data = await response.json(content_type="text/plain")
-
         except (aiohttp.ClientError, aiohttp.http_exceptions.HttpProcessingError) as e:
             logger.error(
                 "aiohttp exception for %s [%s]: %s",
@@ -119,36 +217,12 @@ class DownloadDataScript:
                 getattr(e, "message", None),
                 stack_info=True,
             )
-
         except Exception as e:
             logger.exception(
                 "Non-aiohttp exception occured: %s", getattr(e, "__dict__", {})
             )
-
         else:
-            # Append contest ID as a key-value pair
-            for d in data:
-                # Assuming d["data"] and d["data"]["cartoon"] are always available
-                contest = d["data"]["cartoon"]
-                # Fetch ID (e.g., 660) from contest title (e.g., Contest #660)
-                contest_id = re.search(r"#\d+", contest["title"])
-                if contest_id is not None:
-                    contest_id = int(contest_id.group()[1:])
-                    winners[contest_id] = d
-                else:
-                    logger.warning(
-                        "Unable to find or parse contest ID from this JSON object: %s",
-                        d,
-                    )
-                    winners["unavail_id"].append(d)
-
-        return winners
-
-    async def download_contests(self, url, winners, session):
-        """
-        Fetch and save cartoons, captions and winners of every specified contest.
-        """
-        pdb.set_trace()
+            return response
 
 
 # Execution
